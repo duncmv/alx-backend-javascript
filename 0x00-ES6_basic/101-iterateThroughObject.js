2cf913f48407e847d4e75c66000143fea92ab2d0
duncmv <dtindiwensi@gmail.com>
2024-05-28 16:42:13 +0300

    lint fix

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,7 +1,9 @@
 export default function iterateThroughEmployeesList(employeesList) {
-  const text = employeesList.reduce((str, employee, index) => {
-    return `${str}${employee}${index < employeesList.length - 1 ? ' | ' : ''}`;
-  }, '');
+  const text = employeesList.reduce(
+    (str, employee, index) =>
+      `${str}${employee}${index < employeesList.length - 1 ? ' | ' : ''}`,
+    '',
+  );
 
   return text;
 }

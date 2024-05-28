e5d4f902301ba54270a00073ccfd138ea630d8b0
duncmv <dtindiwensi@gmail.com>
2024-05-28 16:18:21 +0300

    mandatory

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,5 +3,4 @@ export default function handleResponseFromAPI(promise) {
     .then(() => ({status: 200, body: 'success'}))
     .catch(() => new Error())
     .finally(() => console.log('Got response from the API'));
-    
 }
--- 0x01-ES6_promise/new.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-const signUpUser = require('./4-user-promise');
-
-console.log(signUpUser('John', 'Doe'));

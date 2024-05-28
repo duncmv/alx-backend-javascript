37dbcc2d7d0ff177b93db9d93775ee125a27563b
duncmv <dtindiwensi@gmail.com>
2024-05-28 19:58:39 +0300

    lint fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then(() => ({status: 200, body: 'success'}))
-    .then(() => {
+    .then(() => ({ status: 200, body: 'success' }))
+    .then((res) => {
       console.log('Got a response from the API');
+      return res;
     })
     .catch(() => new Error());
 }

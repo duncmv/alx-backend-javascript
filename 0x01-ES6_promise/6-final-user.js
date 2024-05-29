8157b89581f476613616f3f0944a1cfd77707a70
duncmv <dtindiwensi@gmail.com>
2024-05-29 23:12:09 +0300

    lint fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,11 +2,11 @@ import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename)])
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(filename),])
     .then((res) => (
       res.map((result) => ({
         status: result.status,
-        value: result.value === 'fulfilled' ? result.value : String(result.reason)
+        value: result.value === 'fulfilled' ? result.value : `${result.reason.name}: ${result.reason.message}`,
       }))
     ));
 }

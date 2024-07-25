92a5b0f2f87a857f8c904a5b6cb8888883c9f01c
duncmv <dtindiwensi@gmail.com>
2024-07-25 12:26:57 +0300

    first commit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,9 +4,9 @@ const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', function() {
   it('should return the sum of two numbers after rounding', function() {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });

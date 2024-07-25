e51a0f10c60b124ae5536a13f99d753931f3afe9
duncmv <dtindiwensi@gmail.com>
2024-07-25 21:21:05 +0300

    task 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,34 +1,34 @@
-const { expect } = require('chai');
-const calculateNumber = require('./1-calcul');
+const chai = require('chai');
+const calculateNumber = require('./2-calcul_chai.js');
 
 
 describe('calculateNumber', function() {
   describe('SUM', function() {
     it('should return the sum of two numbers after rounding', function() {
-      expect(calculateNumber('SUM', 1, 3).to.equal(4));
-      expect(calculateNumber('SUM', 1, 3.7).to.equal(5));
-      expect(calculateNumber('SUM', 1.2, 3.7).to.equal(5));
-      expect(calculateNumber('SUM', 1.5, 3.7).to.equal(6));
-      expect(calculateNumber('SUM', 1.4999999, 3.499999).to.equal(4));
+      chai.expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+      chai.expect(calculateNumber('SUM', 1, 3.7)).to.equal(5);
+      chai.expect(calculateNumber('SUM', 1.2, 3.7)).to.equal(5);
+      chai.expect(calculateNumber('SUM', 1.5, 3.7)).to.equal(6);
+      chai.expect(calculateNumber('SUM', 1.4999999, 3.499999)).to.equal(4);
     });
   });
   describe('SUBTRACT', function() {
     it('should return the difference of two numbers after rounding', function() {
-      expect(calculateNumber('SUBTRACT', 1, 3).to.equal(-2));
-      expect(calculateNumber('SUBTRACT', 1, 3.7).to.equal(-3));
-      expect(calculateNumber('SUBTRACT', 1.2, 3.7).to.equal(-3));
-      expect(calculateNumber('SUBTRACT', 1.5, 3.7).to.equal(-2));
-      expect(calculateNumber('SUBTRACT', 1.4999999, 3.499999).to.equal(-2));
+      chai.expect(calculateNumber('SUBTRACT', 1, 3)).to.equal(-2);
+      chai.expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
+      chai.expect(calculateNumber('SUBTRACT', 1.2, 3.7)).to.equal(-3);
+      chai.expect(calculateNumber('SUBTRACT', 1.5, 3.7)).to.equal(-2);
+      chai.expect(calculateNumber('SUBTRACT', 1.4999999, 3.499999)).to.equal(-2);
     });
   });
   describe('DIVIDE', function() {
     it('should return the quotient of two numbers after rounding', function() {
-      expect(calculateNumber('DIVIDE', 4, 2).to.equal(2));
-      expect(calculateNumber('DIVIDE', 3.7, 2).to.equal(2));
-      expect(calculateNumber('DIVIDE', 3.7, 1.2).to.equal(4));
-      expect(calculateNumber('DIVIDE', 3.7, 1.5).to.equal(2));
-      expect(calculateNumber('DIVIDE', 3.499999, 1.4999999).to.equal(3));
-      expect(calculateNumber('DIVIDE', 3.7, 0).to.equal('Error'));
+      chai.expect(calculateNumber('DIVIDE', 4, 2)).to.equal(2);
+      chai.expect(calculateNumber('DIVIDE', 3.7, 2)).to.equal(2);
+      chai.expect(calculateNumber('DIVIDE', 3.7, 1.2)).to.equal(4);
+      chai.expect(calculateNumber('DIVIDE', 3.7, 1.5)).to.equal(2);
+      chai.expect(calculateNumber('DIVIDE', 3.499999, 1.4999999)).to.equal(3);
+      chai.expect(calculateNumber('DIVIDE', 3.7, 0)).to.equal('Error');
     });
   });
 });

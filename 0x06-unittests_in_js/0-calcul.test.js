fa5682964a9f1857418dba81466ea143178de973
duncmv <dtindiwensi@gmail.com>
2024-07-25 12:29:48 +0300

    first commit

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,8 +5,14 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', function() {
   it('should return the sum of two numbers after rounding', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+  it('should return the sum of two numbers after rounding', function() {
     assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+  it('should return the sum of two numbers after rounding', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+  it('should return the sum of two numbers after rounding', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });

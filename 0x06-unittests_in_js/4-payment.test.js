21814d82d170717a7110c6dedae4301140e3d2d8
duncmv <dtindiwensi@gmail.com>
2024-07-26 13:20:53 +0300

    task4

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,19 @@
+const Utils = require('./utils.js');
+const sendPaymentToApi = require('./3-payment.js');
+const chai = require('chai');
+const sinon = require('sinon');
+
+
+describe('sendPaymentToApi', function() {
+  it('should send payment to API', function() {
+    const spy = sinon.spy(console)
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    sendPaymentToApi(100, 20);
+    chai.expect(stub.calledOnce).to.be.true;
+    chai.expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    chai.expect(spy.log.calledWith('The total is: 10')).to.be.true;
+    chai.expect(spy.log.calledOnce).to.be.true;
+    stub.restore();
+    spy.log.restore();
+  });
+});
